e06626aee64f362bd9ef60b78632d83bac102983
江宗諭 <108755007@g.nccu.edu.tw>
2022-12-27 16:30:25 +0800

    fix:add letter limit

--- keyword_missoner.py
+++ keyword_missoner.py
@@ -581,4 +581,5 @@ if __name__ == '__main__':
     t_end = time.time()
     t_spent = t_end - t_start
     print(f'finish all routine spent: {t_spent}s')
-    slack_warning().send_letter(f'流量小編{date.strftime("%Y-%m-%d")}/{hour_now}時,本次執行時間為{t_spent}s,已超過50分鐘,請檢查問題')
+    if t_spent >= 3000:
+        slack_warning().send_letter(f'流量小編{date.strftime("%Y-%m-%d")}/{hour_now}時,本次執行時間為{t_spent}s,已超過50分鐘,請檢查問題')
--- slackwarningletter.py
+++ slackwarningletter.py
@@ -6,7 +6,7 @@ class slack_warning:
     def __init__(self):
         load_dotenv()
         self.client = WebClient(token=os.getenv('SLACK_TOKEN'))
-        self.channel = os.getenv('channel_test')
+        self.channel = os.getenv('channel')
     def send_letter(self,text):
         self.client.chat_postMessage(channel=self.channel,text=text)
 
