1d72c80734bb591a4101a94d1830b8cb579a11cb
108755007 <108755007@g.nccu.edu.tw>
2023-02-22 15:27:40 +0800

    feat:add google_ads_py

---
+++ update_googld_ads.py
@@ -0,0 +1,16 @@
+from db import DBhelper
+import pickle
+from jieba_based.jieba_utils import Composer_jieba
+
+
+def fetch_google_ads_keyword():
+    qurey = "SELECT keyword FROM keyword_value WHERE low_price != 0"
+    data = DBhelper('gads').ExecuteSelect(qurey)
+    return [i[0] for i in data]
+
+
+if __name__ == '__main__':
+    jieba = Composer_jieba()
+    google_ads_keyword = fetch_google_ads_keyword()
+    with open(f'{jieba.ROOT_DIR}/jieba_based/google_ads_keyword.pickle', 'wb') as f:
+        pickle.dump(google_ads_keyword, f)
