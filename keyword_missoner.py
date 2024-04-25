dec49846dc43db0e8e9ad32bebf5835fa652c94f
108755007 <108755007@g.nccu.edu.tw>
2024-04-25 16:35:36 +0800

    fix:add blog and media fulltext serch

--- keyword_missoner.py
+++ keyword_missoner.py
@@ -290,6 +290,11 @@ def update_missoner_three_tables(weekday,hour,date=None,n=5000,group = 1,is_UTC0
             #article_list_dict = df_article.to_dict('records')
 
             DBhelper.ExecuteUpdatebyChunk(df_article, db='dione', table='missoner_article', chunk_size=100000, is_ssh=False)
+            if web_id_dict[web_id] != 1:
+                new_df_artice = df_article[['web_id', 'article_id', 'title', 'content', 'pageviews', 'date']]
+                new_df_artice['types'] = web_id_dict[web_id]
+                DBhelper.ExecuteUpdatebyChunk(new_df_artice, db='dione', table='missoner_article_serch', chunk_size=100000,is_ssh=False)
+
             all_article_end = time.time()
 
             time_dict[web_id]['all_article'] = all_article_end - all_article_start
