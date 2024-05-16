7e81a0848d6c5c73f29e1721b22c07fc8672efc8
108755007 <108755007@g.nccu.edu.tw>
2024-05-16 17:50:25 +0800

    fix:update_url

--- updedate_url_config.py
+++ updedate_url_config.py
@@ -4,4 +4,5 @@ import pandas as pd
 if __name__ == '__main__':
     q = f"""SELECT * FROM web_push.web_id_url_encoder_rule"""
     df = pd.DataFrame(DBhelper('jupiter_new').ExecuteSelect(q))
+    DBhelper('dione_2').ExecuteSelect("TRUNCATE TABLE web_id_url_encoder_rule")
     DBhelper.ExecuteUpdatebyChunk(df, db='dione_2', table='web_id_url_encoder_rule', chunk_size=100000, is_ssh=False)
